dd150c3330d043b7cf9deb48c4c62ba958500e8f
alice4599 <alice45992000@gmail.com>
2023-08-15 15:53:16 +0200

    updated open source code

--- src/Weather.js
+++ src/Weather.js
@@ -64,7 +64,7 @@ export default function Weather(props) {
               rel="noreferrer"
             >
               Open-source code
-            </a>
+            </a>{" "}
             by Neli
           </small>
         </div>
